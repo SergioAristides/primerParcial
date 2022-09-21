e4fa23a09d7aa5c73896befeb89b8be358df0bd2
SergioAristides <sergioa.riverab@autonoma.edu.co>
2022-09-21 15:37:04 -0500

    primer pull

---
+++ Parcial.py
@@ -0,0 +1,12 @@
+class Esrtructura:
+    def __init__(self):
+        self.dict={}
+        
+ 
+  
+
+    
+
+
+        
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+from Parcial import Esrtructura
+def main():
+    pass
+    
+if __name__== '__main__':
+    main()
